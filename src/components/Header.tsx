11fde7cd6ba1023502c34386858b205d8ce2de4f
Ryo Sugimoto <sugryo1109+git@gmail.com>
2023-03-22 16:53:45 +0900

    Add link for top page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
 import logo from '../images/site-logo.png'
 const Header = () => {
-    return (
-        <header className="header">
-            <h1 className="header__title">
-                <img src={logo} className="header__title__logo" alt="logo" />
-            </h1>
-        </header>
-    )
+  return (
+    <header className='header'>
+      <h1 className='header__title'>
+        <a href='/'>
+          <img src={logo} className='header__title__logo' alt='logo' />
+        </a>
+      </h1>
+    </header>
+  )
 }
 export default Header
