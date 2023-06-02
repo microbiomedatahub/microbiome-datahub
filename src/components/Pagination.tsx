d4fa7b20d69691686592921b638a9a61d0a03d8d
Ryo Sugimoto <sugryo1109+gnupg@gmail.com>
2023-06-02 14:02:55 +0900

    Fix pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,57 +1,99 @@
-import { useMemo } from 'react'
 import { Link } from 'react-router-dom'
 
-const Pagination = ({ currentPage, lastPage }: { currentPage: number; lastPage: number }) => {
-  const paginationCurrentPage = (index: number) => {
-    return 'pagination__item' + (currentPage === index ? ' current' : '')
+const FirstPagination = ({ currentPage }: { currentPage: number }) => {
+  if (currentPage === 1) {
+    return <li className='pagination__item current'>1</li>
+  } else {
+    return <Link className='pagination__item' to='?page=1'>1</Link>
   }
-  const paginationItems: Array<number> = useMemo(() => {
-    const paginationList = []
-    for (let i = 2; i < lastPage; i++) {
-      paginationList.push(i)
-    }
-    return paginationList
-  }, [lastPage])
+}
+
+const FirstDot = (
+  { currentPage, lastPage }: { currentPage: number; lastPage: number },
+) => {
+  if (lastPage < 4 || currentPage < 3) {
+    return null
+  }
 
   return (
-    <ol className='pagination' aria-label='ページャー'>
-      <li className={paginationCurrentPage(1)}>
-        {currentPage !== 1 ? <Link to='?page=1'>1</Link> : 1}
-      </li>
-      {currentPage - 1 !== 1 && currentPage !== 1 && (
-        <>
-          <div className='pagination__item dot'>
-            <svg viewBox='0 0 31 4' xmlns='http://www.w3.org/2000/svg'>
-              <circle cx='2' cy='2' r='2' fill='#D9D9D9' />
-              <circle cx='11' cy='2' r='2' fill='#D9D9D9' />
-              <circle cx='20' cy='2' r='2' fill='#D9D9D9' />
-              <circle cx='29' cy='2' r='2' fill='#D9D9D9' />
-            </svg>
-          </div>
-        </>
-      )}
-      {paginationItems.map((item: number) => {
-        return (
-          <li className={paginationCurrentPage(item)} key={item}>
-            {currentPage !== item ? <Link to={`?page=${item}`}>{item}</Link> : item}
-          </li>
-        )
+    <>
+      <div className='pagination__item dot'>
+        <svg viewBox='0 0 31 4' xmlns='http://www.w3.org/2000/svg'>
+          <circle cx='2' cy='2' r='2' fill='#D9D9D9' />
+          <circle cx='11' cy='2' r='2' fill='#D9D9D9' />
+          <circle cx='20' cy='2' r='2' fill='#D9D9D9' />
+          <circle cx='29' cy='2' r='2' fill='#D9D9D9' />
+        </svg>
+      </div>
+    </>
+  )
+}
+
+const CenterPaginationItems = (
+  { currentPage, lastPage }: { currentPage: number; lastPage: number },
+) => {
+  if (lastPage < 3) {
+    return null
+  }
+
+  const paginationNumbers = [currentPage - 1, currentPage, currentPage + 1].filter((item) =>
+    item > 1 && item < lastPage
+  )
+
+  return (
+    <>
+      {paginationNumbers.map((item) => {
+        if (item === currentPage) {
+          return <li key={item} className='pagination__item current'>{item}</li>
+        } else {
+          return <Link key={item} className='pagination__item' to={`?page=${item}`}>{item}</Link>
+        }
       })}
-      {currentPage + 1 !== lastPage && currentPage !== lastPage && (
-        <>
-          <div className='pagination__item dot'>
-            <svg viewBox='0 0 31 4' xmlns='http://www.w3.org/2000/svg'>
-              <circle cx='2' cy='2' r='2' fill='#D9D9D9' />
-              <circle cx='11' cy='2' r='2' fill='#D9D9D9' />
-              <circle cx='20' cy='2' r='2' fill='#D9D9D9' />
-              <circle cx='29' cy='2' r='2' fill='#D9D9D9' />
-            </svg>
-          </div>
-        </>
-      )}
-      {lastPage !== 1 && <li className={paginationCurrentPage(lastPage)}>
-        {currentPage !== lastPage ? <Link to={`?page=${lastPage}`}>{lastPage}</Link> : lastPage}
-      </li>}
+    </>
+  )
+}
+
+const LastDot = (
+  { currentPage, lastPage }: { currentPage: number; lastPage: number },
+) => {
+  if (lastPage < 4 || ((currentPage + 1) === lastPage || currentPage === lastPage)) {
+    return null
+  }
+
+  return (
+    <>
+      <div className='pagination__item dot'>
+        <svg viewBox='0 0 31 4' xmlns='http://www.w3.org/2000/svg'>
+          <circle cx='2' cy='2' r='2' fill='#D9D9D9' />
+          <circle cx='11' cy='2' r='2' fill='#D9D9D9' />
+          <circle cx='20' cy='2' r='2' fill='#D9D9D9' />
+          <circle cx='29' cy='2' r='2' fill='#D9D9D9' />
+        </svg>
+      </div>
+    </>
+  )
+}
+
+const LastPagination = ({ currentPage, lastPage }: { currentPage: number; lastPage: number }) => {
+  if (lastPage === 1) {
+    return null
+  }
+
+  if (currentPage === lastPage) {
+    return <li className='pagination__item current'>{lastPage}</li>
+  } else {
+    return <Link className='pagination__item' to={`?page=${lastPage}`}>{lastPage}</Link>
+  }
+}
+
+const Pagination = ({ currentPage, lastPage }: { currentPage: number; lastPage: number }) => {
+  return (
+    <ol className='pagination' aria-label='ページャー'>
+      <FirstPagination currentPage={currentPage} />
+      <FirstDot currentPage={currentPage} lastPage={lastPage} />
+      <CenterPaginationItems currentPage={currentPage} lastPage={lastPage} />
+      <LastDot currentPage={currentPage} lastPage={lastPage} />
+      <LastPagination currentPage={currentPage} lastPage={lastPage} />
     </ol>
   )
 }
