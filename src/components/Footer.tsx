ac741759418b2d35eff779a5918cad4010b18753
soutaito <soutaito@info-lounge.jp>
2023-06-30 13:34:18 +0900

    Update metatag and footer link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,10 @@ const Footer = () => {
   return (
     <footer className='footer'>
       <nav className='footer__navigation'>
-        <a href='' title='About us' className='footer__navigation__link'>About us</a>
-        <a href='' title='FAQ' className='footer__navigation__link'>FAQ</a>
-        <a href='' title='Terms and Use' className='footer__navigation__link'>Terms and Use</a>
-        <a href='' title='Contact us' className='footer__navigation__link'>Contact us</a>
+        <a href='https://mdatahub.org/document/#about_us' title='About us' className='footer__navigation__link'>About us</a>
+        <a href='https://mdatahub.org/document/#faq' title='FAQ' className='footer__navigation__link'>FAQ</a>
+        <a href='https://mdatahub.org/document/#terms_and_use' title='Terms and Use' className='footer__navigation__link'>Terms and Use</a>
+        <a href='https://mdatahub.org/document/#' title='Contact us' className='footer__navigation__link'>Contact us</a>
       </nav>
       <p className='footer__copyright'>
         © MicrobeDB.JP project team 2019 | Except where otherwise noted, content on this site is licensed under a
