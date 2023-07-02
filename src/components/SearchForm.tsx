4c6164bb718b2f961619b081ee4c8694d5722785
Ryo Sugimoto <sugryo1109+gnupg@gmail.com>
2023-07-03 08:16:58 +0900

    Unuse useAtom

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,7 +1,7 @@
-import { useAtom, useAtomValue } from 'jotai'
+import { useAtomValue } from 'jotai'
 import { useEffect, useMemo, useState } from 'react'
-import { resultsCountTotalAtom, selectModeAtom } from '../store/store'
 import { useSearchParams } from 'react-router-dom'
+import { resultsCountTotalAtom, selectModeAtom } from '../store/store'
 
 const SearchForm = () => {
   const countTotal = useAtomValue(resultsCountTotalAtom)
@@ -11,7 +11,7 @@ const SearchForm = () => {
     setOrderBy('dateCreated')
   }, [selectMode])
 
-  const [order, setOrder] = useState<'asc'|'desc'>('desc')
+  const [order, setOrder] = useState<'asc' | 'desc'>('desc')
   const [searchParams, setSearchParams] = useSearchParams()
 
   useEffect(() => {
@@ -23,7 +23,7 @@ const SearchForm = () => {
       sortOrder = 'desc'
     }
     if (sortOrder !== '' && sortOrder !== order) {
-      setOrder(sortOrder as 'asc'|'desc')
+      setOrder(sortOrder as 'asc' | 'desc')
     }
 
     const sortBy = searchParams.get('sort')?.slice(0, -1)
@@ -34,15 +34,15 @@ const SearchForm = () => {
 
   const sortKeyValues = useMemo(() => {
     const sorts = [
-      {key: 'dateCreated', value: 'Date Created'},
-      {key: 'dateModified', value: 'Date Modified'},
-      {key: 'datePublished', value: 'Date Published'},
+      { key: 'dateCreated', value: 'Date Created' },
+      { key: 'dateModified', value: 'Date Modified' },
+      { key: 'datePublished', value: 'Date Published' },
       // {key: '_annotation.date_size', value: 'Data Size'},→→ ElasticSearchで数値に変更しないと対応できない
     ]
     if (selectMode === 'project') {
-      sorts.push({key: 'identifier.keyword', value: 'Project ID'})
+      sorts.push({ key: 'identifier.keyword', value: 'Project ID' })
     } else if (selectMode === 'genome') {
-      sorts.push({key: 'identifier.keyword', value: 'Genome ID'})
+      sorts.push({ key: 'identifier.keyword', value: 'Genome ID' })
     }
     return sorts
   }, [selectMode])
@@ -51,8 +51,8 @@ const SearchForm = () => {
     setOrderBy(e.currentTarget.value)
   }
 
-  useEffect(()  => {
-    const queries: {[key: string]: string} = {}
+  useEffect(() => {
+    const queries: { [key: string]: string } = {}
     searchParams.delete('sort')
     for (const [key, value] of searchParams.entries()) {
       queries[key] = value
@@ -83,12 +83,18 @@ const SearchForm = () => {
         })}
       </select>
       <div className='search__order'>
-        <button onClick={(_e) => setOrder('asc')} className={`search__order__button${(order === 'asc' ? ' active' : '')}`}>
+        <button
+          onClick={(_e) => setOrder('asc')}
+          className={`search__order__button${(order === 'asc' ? ' active' : '')}`}
+        >
           <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 12 8'>
             <path d='M10.59 7.42L6 2.83L1.41 7.42L0 6L6 -1.90735e-06L12 6L10.59 7.42Z' fill='#D9D9D9' />
           </svg>
         </button>
-        <button onClick={(_e) => setOrder('desc')} className={`search__order__button${(order === 'desc' ? ' active' : '')}`}>
+        <button
+          onClick={(_e) => setOrder('desc')}
+          className={`search__order__button${(order === 'desc' ? ' active' : '')}`}
+        >
           <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 12 8'>
             <path d='M1.41 0.580002L6 5.17L10.59 0.580002L12 2L6 8L0 2L1.41 0.580002Z' fill='#D9D9D9' />
           </svg>
