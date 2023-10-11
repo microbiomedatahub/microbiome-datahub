b53b0ae5cc2b901b5b1d58cab2dc0dc0696e2e3b
Ryo Sugimoto <sugryo1109+gnupg@gmail.com>
2023-10-11 15:07:34 +0900

    Use 'Microbiome Datahub' in copyright credit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,15 +2,24 @@ import logo from '../images/site-logo-bk.png'
 const Footer = () => {
   return (
     <footer className='footer'>
-      <img src={logo} alt="Logo:Microbiome Datahub" className="footer__logo"/>
+      <img src={logo} alt='Logo:Microbiome Datahub' className='footer__logo' />
       <nav className='footer__navigation'>
-        <a href='https://mdatahub.org/document/#about_us' title='About us' className='footer__navigation__link'>About us</a>
+        <a href='https://mdatahub.org/document/#about_us' title='About us' className='footer__navigation__link'>
+          About us
+        </a>
         <a href='https://mdatahub.org/document/#faq' title='FAQ' className='footer__navigation__link'>FAQ</a>
-        <a href='https://mdatahub.org/document/#terms_and_use' title='Terms and Use' className='footer__navigation__link'>Terms and Use</a>
+        <a
+          href='https://mdatahub.org/document/#terms_and_use'
+          title='Terms and Use'
+          className='footer__navigation__link'
+        >
+          Terms and Use
+        </a>
         <a href='https://mdatahub.org/document/#' title='Contact us' className='footer__navigation__link'>Contact us</a>
       </nav>
       <p className='footer__copyright'>
-        © MicrobeDB.JP project team 2019 | Except where otherwise noted, content on this site is licensed under a Creative Commons Attribution 4.0 International license (CC-BY-4.0)
+        © Microbiome Datahub | Except where otherwise noted, content on this site is licensed under a Creative Commons
+        Attribution 4.0 International license (CC-BY-4.0)
       </p>
     </footer>
   )
