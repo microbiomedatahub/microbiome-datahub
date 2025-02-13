4a1a57cadf8963d9074bbafecbb7c1db6d31abfe
Yumie Matsumoto <matsumoto@info-lounge.jp>
2025-02-13 16:54:03 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,21 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8081',
-        changeOrigin: true
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production'
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: isProduction ? 'https://mdatahub.org' : 'http://localhost:8081',
+          changeOrigin: true
+        },
+        '/data': {
+          target: isProduction ? 'https://mdatahub.org' : 'http://localhost:8081',
+          changeOrigin: true
+        }
       }
     }
   }
